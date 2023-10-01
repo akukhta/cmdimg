25fb78900a2f7ce5ef1df549a3ab21abef87aa49
Alexey Kuhta <electricsix23@gmail.com>
2023-10-01 17:34:29 +0200

    Update bootstrap_x86_debug.bat

--- bootstrap_x86_debug.bat
+++ bootstrap_x86_debug.bat
@@ -1,4 +1,4 @@
 mkdir bin
-cmake . -B .\bin\ -A Win32 -DCMAKE_BUILD_TYPE=Debug
+cmake . -B .\bin\ -A Win32 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 cd bin
 cmake --build .
